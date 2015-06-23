f62706b93b3365f008df8363a83200f57b7bb7c6
mars <martin@rebel-it.com>
2015-06-23 18:06:14 +0100

    reset program name

--- heavy.sh
+++ heavy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # make CONFIG=Release Build/heavy.elf && rm Build/HeavyProgram.o && make CONFIG=Debug Build/heavy.elf Build/heavy.syx
+rm -f Build/startup.o
+echo '.string "Heavy"' > Source/progname.s
 
 make CONFIG=Release Build/heavy.elf
