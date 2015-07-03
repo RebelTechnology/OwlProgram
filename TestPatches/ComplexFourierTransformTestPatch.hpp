e22af35ac71b5d9d757b293968f9641f3d9ffb0f
giuliomoro <giuliomoro@yahoo.it>
2015-07-03 14:54:22 +0100

    Removed useless code from ComplexFourierTransformTest

--- TestPatches/ComplexFourierTransformTestPatch.hpp
+++ TestPatches/ComplexFourierTransformTestPatch.hpp
@@ -40,46 +40,19 @@ private:
   ComplexFloatArray inout;
 public:
   ComplexFourierTransformTestPatch(){
-     registerParameter(PARAMETER_A, "Toggle FFT method");
-    // success=true;
-    // passed=0;
-    // failed=0;
     fftsize=getBlockSize();
     fftArm.init(fftsize);
-    // fftGeneric.init(fftsize);
     inout=ComplexFloatArray::create(fftsize);
     
   };
   void processAudio(AudioBuffer &buffer){
     FloatArray samples=buffer.getSamples(0);
     inout.copyFrom(samples); //copies only real values 
-    if(getParameterValue(PARAMETER_A)>0.5){
-      debugMessage("generic");
-      // fftGeneric.fft(inout);
-      // fftGeneric.ifft(inout);
-    } else {
-      debugMessage("arm");
-      fftArm.fft(inout);
-      fftArm.ifft(inout);
-    }
+    fftArm.fft(inout);
+    fftArm.ifft(inout);
     inout.copyTo(samples);
     samples.scale(0.2);
 
-    /*
-    float *sig=buffer.getSamples(0);
-    if(success==false){
-      for(int n=0; n<getBlockSize(); n++){
-        sig[n]+=0.05*rand()/(float)RAND_MAX;
-      }
-    } else {
-      static float phase=0;
-      float inc=2*M_PI/200.0f;
-      for(int n=0; n<getBlockSize(); n++){
-        sig[n]+=0.2*sinf(phase);
-        phase+=inc;
-        phase= phase>2*M_PI ? phase-2*M_PI : phase;
-      }
-    }*/
   }
 };
 
