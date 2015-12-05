d29dc6ad0628cddfe4cd1ccadd4acafde7bc3997
Martin Klang <martin@rebel-it.com>
2015-12-05 05:38:42 +0000

    Update PatchParameter.hpp

--- LibSource/PatchParameter.hpp
+++ LibSource/PatchParameter.hpp
@@ -1,7 +1,8 @@
 #include "SmoothValue.hpp"
 
 class PatchParameter {
-  virtual void update(uint16_t value);
+public:
+  virtual void update(uint16_t value){}
 };
 
 template<type T, type V>
@@ -26,41 +27,43 @@ public:
 };
 
 template<type T, type V>
-PatchParameter<T, V> Patch::requestParameter(const char* name, T min, T max, V defaultValue = V()){
+GenericParameter<T, V> Patch::requestParameter(const char* name, T min, T max, V defaultValue = V()){
   int pid = 0;
   if(nextPid < 5){
     pid = nextPid++;
     registerParameter(pid, name);
   }
-  return PatchParameter<T, V>(PatchParameterId(pid), min, max, defaultValue);
+  return GenericParameter<T, V>(PatchParameterId(pid), min, max, defaultValue);
 }
+
 template<type T>
-PatchParameter<T, SmoothValue<T>> Patch::requestParameter(const char* name, T min, T max, T defaultValue, T lambda){
+GenericParameter<T, SmoothValue<T>> Patch::requestParameter(const char* name, T min, T max, T defaultValue, T lambda){
   requestParameter(name, min, max, SmoothValue<T>(defaultValue, lambda));
 }
 
-typedef IntParameter PatchParameter<int, int>;
-typedef StiffIntParameter PatchParameter<int, StiffInt>;
-typedef SmoothIntParameter PatchParameter<int, SmoothInt>;
-typedef FloatParameter PatchParameter<float, SmoothFloat>;
+// Patch {
+// FloatParameter requestParameter(const char* name, float min, float max, float defaultValue);
+// SmoothFloatParameter requestParameter(const char* name, float min, float max, float defaultValue, float lambda);
+// IntParameter requestParameter(const char* name, int min, int max, int defaultValue);
+// SmoothIntParameter requestParameter(const char* name, int min, int max, int defaultValue, int delta);
+// };
+
+// exponentially averaged float parameter with hysteresis:
+// StiffValue<SmoothFloatParameter> spread(0.1); spread = requestParameter("spread", 0.0, 1.0, 0.5, 0.9);
+// typedef StiffSmoothFloatParameter StiffValue<SmoothFloatParameter> ??
+
+// default in patch processor to StiffSmoothIntParameter
+
+typedef IntParameter GenericParameter<int, int>;
+typedef StiffIntParameter GenericParameter<int, StiffInt>;
+typedef SmoothIntParameter GenericParameter<int, SmoothInt>;
+typedef FloatParameter GenericParameter<float, float;
+typedef StiffFloatParameter GenericParameter<float, StiffFloat>;
+typedef SmoothFloatParameter GenericParameter<float, SmoothFloat>;
 //FloatParameter spread = requestParameter("Spread", 0.0, 1.0, 0.5);
 //SmoothFloatParameter spread = requestParameter("Spread", 0.0, 1.0, SmoothFloat(0.9, 0.5));
 //StiffFloatParameter spread = requestParameter("Spread", 0.0, 1.0);
 //SmoothFloatParameter spread = requestParameter("Spread", 0.0, 1.0, 0.5, 0.9);
-
-template<>
-IntParameter Patch::requestParameter(const char* name, int min, int max, int lambda=5){
-  // will need copy constructor for SmoothValue (default/trivial ok)
-  if(nextPid < 5)
-    return IntParameter(PatchParameterId(nextPid++), min, max, SmoothInt(lambda, 0));
-  return IntParameter();
-}
-
-FloatParameter Patch::requestParameter(const char* name, float min, float max, float lambda=0.8){
-  if(nextPid < 5)
-    return FloatParameter(PatchParameterId(nextPid++), min, max, SmoothFloat(lambda, 0));
-  return FloatParameter();
-}
 
 template<type T, type V>
 GenericParameter<T,V>::GenericParameter<T,V>(PatchParameterId p, T mn, T mx, T v)
