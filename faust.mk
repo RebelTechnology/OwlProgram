60cb45700a8a10b3887b60a00212169702bbfa0c
mars <martin@rebel-it.com>
2016-02-16 14:22:18 +0000

    hard coded output filename

--- faust.mk
+++ faust.mk
@@ -1,5 +1,9 @@
 
-faust: $(BUILD)/Source/$(PATCHFILE)
+faust: $(BUILD)/Source/FaustPatch.hpp
+
+$(BUILD)/Source/FaustPatch.hpp: $(PATCHSOURCE)/$(FAUST).dsp
+	faust -I $(PATCHSOURCE) -i -inpl -a owl.cpp -cn GeneratedPatch $< -o $@
 
 $(BUILD)/Source/%Patch.hpp: $(PATCHSOURCE)/%.dsp
-	@faust -I $(PATCHSOURCE) -i -inpl -a owl.cpp -cn $(PATCHNAME)Patch $< -o $@
+	faust -I $(PATCHSOURCE) -i -inpl -a owl.cpp -cn GeneratedPatch $< -o $@
+
