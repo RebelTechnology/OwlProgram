ef7180373f3f0ad3949cdacd55a619fa32bb7697
mars <martin@rebel-it.com>
2016-05-19 10:18:08 +0100

    added dependencies

--- compile.mk
+++ compile.mk
@@ -5,7 +5,7 @@ CPP_SRC = main.cpp operators.cpp message.cpp StompBox.cpp PatchProcessor.cpp
 CPP_SRC += FloatArray.cpp ComplexFloatArray.cpp FastFourierTransform.cpp 
 CPP_SRC += Envelope.cpp VoltsPerOctave.cpp
 CPP_SRC += WavetableOscillator.cpp PolyBlepOscillator.cpp
-CPP_SRC += PatchProgram.cpp
+CPP_SRC += PatchProgram.cpp SmoothValue.cpp PatchParameter.cpp
 
 SOURCE       = $(BUILDROOT)/Source
 LIBSOURCE    = $(BUILDROOT)/LibSource
