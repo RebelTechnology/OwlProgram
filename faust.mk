c6c329461ae123142391a982b06527d170c7bcc9
mars <martin@rebel-it.com>
2016-06-25 00:11:59 +0100

    added local copy of FAUST owl.cpp architecture

--- faust.mk
+++ faust.mk
@@ -2,8 +2,8 @@
 faust: $(BUILD)/Source/FaustPatch.hpp
 
 $(BUILD)/Source/FaustPatch.hpp: $(PATCHSOURCE)/$(FAUST).dsp
-	@faust -I $(PATCHSOURCE) -i -inpl -a owl.cpp -cn GeneratedPatch $< -o $@
+	@faust -I $(PATCHSOURCE) -i -inpl -a FaustCode/owl.cpp -cn GeneratedPatch $< -o $@
 
 $(BUILD)/Source/%Patch.hpp: $(PATCHSOURCE)/%.dsp
-	@faust -I $(PATCHSOURCE) -i -inpl -a owl.cpp -cn GeneratedPatch $< -o $@
+	@faust -I $(PATCHSOURCE) -i -inpl -a FaustCode/owl.cpp -cn GeneratedPatch $< -o $@
 
