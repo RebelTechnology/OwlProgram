262da549371edee171cf4bdef65adb60e0fa4e18
pingdynasty <martin@rebel-it.com>
2016-07-31 12:18:33 +0000

    added methods for fm

--- LibSource/Oscillator.h
+++ LibSource/Oscillator.h
@@ -8,10 +8,17 @@ public:
   Oscillator(){}
   virtual ~Oscillator(){}
   virtual float getNextSample() = 0;
+  virtual float getNextSample(float fm){
+    return getNextSample();
+  }
   /* Fills @param output with samples */
   virtual void getSamples(FloatArray output){
     for(int i=0; i<output.getSize(); ++i)
       output[i] = getNextSample();
+  }
+  virtual void getSamples(FloatArray output, FloatArray fm){
+    for(int i=0; i<output.getSize(); ++i)
+      output[i] = getNextSample(fm[i]);
   }
   virtual void setSampleRate(float value){}
   virtual void setFrequency(float value){}
