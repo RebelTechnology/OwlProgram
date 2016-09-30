1471aad066806eca1d8382b778280e05ce65cf53
mars <martin@rebel-it.com>
2016-09-30 10:59:58 +0100

    updated Control

--- LibSource/Control.h
+++ LibSource/Control.h
@@ -9,14 +9,18 @@ template<PatchParameterId PID>
 class Control {
 public:
   Control<PID>(const float value=0){
-  ASSERT(PID < getProgramVector()->parameters_size, "Invalid parameter ID");
-  set(value);
+    ASSERT(PID < getProgramVector()->parameters_size, "Invalid parameter ID");
+    set(value);
   }
   void set(const float value){
     doSetPatchParameter(PID, (int16_t)(value*4096));
   }
   float get(){
-    return getProgramVector()->parameters[PID]/4096.0f;
+    if(getProgramVector()->hardware_version == OWL_MODULAR_HARDWARE && PID < 4){
+      return (4095 - getProgramVector()->parameters[PID])/4096.0f;
+    }else{
+      return getProgramVector()->parameters[PID]/4096.0f;
+    }
   }
   Control<PID>& operator=(const float value){
     set(value);
