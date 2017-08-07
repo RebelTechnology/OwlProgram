f33070c75059f186ec65a88eec42c5d8d7e884bd
mars <martin@rebel-it.com>
2017-08-07 16:10:59 +0100

    sample conversion for wm8731 samples

--- Source/SampleBuffer.hpp
+++ Source/SampleBuffer.hpp
@@ -8,66 +8,63 @@
 #ifdef ARM_CORTEX
 #include "arm_math.h"
 #endif //ARM_CORTEX
-
 class SampleBuffer : public AudioBuffer {
 protected:
+  // FloatArray left;//[AUDIO_MAX_BLOCK_SIZE];
+  // FloatArray right;//[AUDIO_MAX_BLOCK_SIZE];
   float left[AUDIO_MAX_BLOCK_SIZE];
   float right[AUDIO_MAX_BLOCK_SIZE];
   uint16_t size;
 public:
-  void split(int32_t* input, uint16_t blocksize){
-    // ASSERT((blocksize & 0x3) == 0, "invalid blocksize");
+  void split(int32_t* data, uint16_t blocksize){
+    uint16_t* input = (uint16_t*)data;
     size = blocksize;
     float* l = left;
     float* r = right;
-    uint32_t cnt = size >> 1u; // *2/4
-    while(cnt > 0u){
-      *l++ = (float)(int32_t)((*input++)<<8) / 2147483648.0f;
-      *r++ = (float)(int32_t)((*input++)<<8) / 2147483648.0f;
-      *l++ = (float)(int32_t)((*input++)<<8) / 2147483648.0f;
-      *r++ = (float)(int32_t)((*input++)<<8) / 2147483648.0f;
-      cnt--;
+    uint32_t blkCnt = size;
+    int32_t qint;
+    while(blkCnt > 0u){
+      qint = (*input++)<<16;
+      qint |= *input++;
+      *l++ = qint / 2147483648.0f;
+      qint = (*input++)<<16;
+      qint |= *input++;
+      *r++ = qint / 2147483648.0f;
+      // *l++ = (int32_t)((*input++)<<16|*++input) / 2147483648.0f;
+      // *r++ = (int32_t)((*input++)<<16|*++input) / 2147483648.0f;
+      // *l++ = (int32_t)((*input++)<<16|*++input) / 2147483648.0f;
+      // *r++ = (int32_t)((*input++)<<16|*++input) / 2147483648.0f;
+      blkCnt--;
     }
   }
   void comb(int32_t* output){
-    int32_t* dest = output;
-    int32_t tmp;
-    // Seems CS4271 ADC samples are signed, DAC are unsigned. I2S Standard mode.
-    for(int i=0; i<size; ++i){
-      // tmp = (int32_t)(left[i] * 0x800000);
-      // *dest++ = (uint32_t)(tmp+0x800000);
-      // tmp = (int32_t)(right[i] * 0x800000);
-      // *dest++ = (uint32_t)(tmp+0x800000);
-      tmp = ((int32_t)(left[i] * 2147483648.0f));
-      *dest++ = tmp>>8;
-      tmp = ((int32_t)(right[i] * 2147483648.0f));
-      *dest++ = tmp>>8;
-    }
-    // float* l = left;
-    // float* r = right;
-    // uint32_t cnt = size >> 1u; // *2/4
-    // while(cnt > 0u){
-    //   // *output++ = ((uint32_t)(*l++ * 2147483648.0f))>>8;
-    //   // *output++ = ((uint32_t)(*r++ * 2147483648.0f))>>8;
-    //   // *output++ = ((uint32_t)(*l++ * 2147483648.0f))>>8;
-    //   // *output++ = ((uint32_t)(*r++ * 2147483648.0f))>>8;
-    //   *output++ = ((uint32_t)(*l++ * 8388608.0f));
-    //   *output++ = ((uint32_t)(*r++ * 8388608.0f));
-    //   *output++ = ((uint32_t)(*l++ * 8388608.0f));
-    //   *output++ = ((uint32_t)(*r++ * 8388608.0f));
-    //   cnt--;
-    // }
-  }
-  void set(float value){
-    for(int i=0; i<size; ++i){
-      left[i] = value;
-      right[i] = value;
+    float* l = left;
+    float* r = right;
+    uint32_t blkCnt = size;
+    uint16_t* dst = (uint16_t*)output;
+    int32_t qint;
+    while(blkCnt > 0u){
+#ifdef AUDIO_SATURATE_SAMPLES
+      qint = clip_q63_to_q31((q63_t)(*l++ * 2147483648.0f));
+      *dst++ = qint >> 16;
+      *dst++ = qint & 0xffff;
+      qint = clip_q63_to_q31((q63_t)(*r++ * 2147483648.0f));
+      *dst++ = qint >> 16;
+      *dst++ = qint & 0xffff;
+#else /* AUDIO_SATURATE_SAMPLES */
+      qint = *l++ * 2147483648.0f;
+      *dst++ = qint >> 16;
+      *dst++ = qint & 0xffff;
+      qint = *r++ * 2147483648.0f;
+      *dst++ = qint >> 16;
+      *dst++ = qint & 0xffff;
+#endif /* AUDIO_SATURATE_SAMPLES */
+      blkCnt--;
     }
   }
   void clear(){
-    set(0);
-    // memset(left, 0, getSize()*sizeof(float));
-    // memset(right, 0, getSize()*sizeof(float));
+    memset(left, 0, getSize()*sizeof(float));
+    memset(right, 0, getSize()*sizeof(float));
   }
   inline FloatArray getSamples(int channel){
     return channel == 0 ? FloatArray(left, size) : FloatArray(right, size);
@@ -76,7 +73,7 @@ public:
   //   return channel == 0 ? left : right;
   // }
   inline int getChannels(){
-    return 2;
+    return AUDIO_CHANNELS;
   }
   // void setSize(uint16_t sz){
   // // size is set by split()
