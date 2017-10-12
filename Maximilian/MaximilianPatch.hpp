38781bea26c3946d7a55d7c0b47e195a8f49a6dd
mars <martin@rebel-it.com>
2017-10-12 13:33:29 +0100

    cleaned up comments

--- Maximilian/MaximilianPatch.hpp
+++ Maximilian/MaximilianPatch.hpp
@@ -7,28 +7,7 @@
 class MaximilianPatch : public Patch {
 private:
 #include "maximilian-patch.h"
-// #include "maximilian_examples/1.TestTone.cpp"
-// #include "maximilian_examples/2.TwoTones.cpp"
-// #include "maximilian_examples/3.AM1.cpp"
-// #include "maximilian_examples/4.AM2.cpp"
-// #include "maximilian_examples/5.FM1.cpp"
-// #include "maximilian_examples/6.FM2.cpp"
-// #include "maximilian_examples/7.Counting1.cpp"
-// #include "maximilian_examples/8.Counting2.cpp"
-// #include "maximilian_examples/8.Counting3.cpp"
-// #include "maximilian_examples/8.Counting4.cpp"
-// #include "maximilian_examples/9.Envelopes.cpp"
-// #include "maximilian_examples/10.Filters.cpp"
-// #include "maximilian_examples/11.Mixing.cpp"
-// #include "maximilian_examples/13.Advanced-Filters.cpp"
-// #include "maximilian_examples/14.monosynth.cpp" // 32%
-// #include "maximilian_examples/15.polysynth.cpp" // 188%
-// #include "maximilian_examples/16.Replicant.cpp" // 110%
-// #include "maximilian_examples/17.Compressor.cpp" // missing maxiSample::load
-// #include "maximilian_examples/19.Enveloping2.cpp" // missing maxiSample::load
-// #include "maximilian_examples/20.FFT_example.cpp" // missing/failing FFT lib
   float output[2];
-
 public:
   MaximilianPatch(){
     setup();
