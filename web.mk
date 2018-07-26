768f0974cb892468bb6b39154ac324cf210d01a7
Martin Klang <martin@rebel-it.com>
2018-07-26 14:26:54 +0100

    updated web build to compile individual files to bitcode before linking (tested with emcc v1.38.10)

--- web.mk
+++ web.mk
@@ -16,17 +16,43 @@ EMCCFLAGS += -Wno-c++11-extensions
 EMCCFLAGS += --memory-init-file 0 # don't create separate memory init file .mem
 EMCCFLAGS += -s EXPORTED_FUNCTIONS="['_WEB_setup','_WEB_setParameter','_WEB_processBlock','_WEB_getPatchName','_WEB_getParameterName','_WEB_getMessage','_WEB_getStatus','_WEB_getButtons','_WEB_setButtons', '_WEB_processMidi']"
 EMCCFLAGS += -s "EXTRA_EXPORTED_RUNTIME_METHODS=['cwrap']"
-EMCC_SRC   = $(SOURCE)/PatchProgram.cpp $(SOURCE)/PatchProcessor.cpp $(SOURCE)/message.cpp
-EMCC_SRC  += WebSource/web.cpp
-EMCC_SRC  += $(LIBSOURCE)/basicmaths.c $(LIBSOURCE)/Patch.cpp $(LIBSOURCE)/FloatArray.cpp $(LIBSOURCE)/ComplexFloatArray.cpp $(LIBSOURCE)/FastFourierTransform.cpp $(LIBSOURCE)/Envelope.cpp $(LIBSOURCE)/VoltsPerOctave.cpp $(LIBSOURCE)/Window.cpp $(LIBSOURCE)/WavetableOscillator.cpp $(LIBSOURCE)/PolyBlepOscillator.cpp $(LIBSOURCE)/SmoothValue.cpp
-# EMCC_SRC  += $(LIBSOURCE)/fastpow.c $(LIBSOURCE)/fastlog.c $(LIBSOURCE)/system_tables.cpp
-EMCC_SRC  += $(PATCH_CPP_SRC) $(PATCH_C_SRC)
-EMCC_SRC  += Libraries/KissFFT/kiss_fft.c
-EMCC_SRC  += $(wildcard $(GENSOURCE)/*.c)
-EMCC_SRC  += $(wildcard $(GENSOURCE)/*.cpp)
-EMCC_SRC  += $(wildcard $(PATCHSOURCE)/*.c)
-EMCC_SRC  += $(wildcard $(PATCHSOURCE)/*.cpp)
-WEBDIR     = $(BUILD)/web
+CPP_SRC  = $(SOURCE)/PatchProgram.cpp $(SOURCE)/PatchProcessor.cpp $(SOURCE)/message.cpp
+CPP_SRC += WebSource/web.cpp
+C_SRC   += $(LIBSOURCE)/basicmaths.c
+CPP_SRC += $(LIBSOURCE)/Patch.cpp $(LIBSOURCE)/FloatArray.cpp $(LIBSOURCE)/ComplexFloatArray.cpp $(LIBSOURCE)/FastFourierTransform.cpp $(LIBSOURCE)/Envelope.cpp $(LIBSOURCE)/VoltsPerOctave.cpp $(LIBSOURCE)/Window.cpp $(LIBSOURCE)/WavetableOscillator.cpp $(LIBSOURCE)/PolyBlepOscillator.cpp $(LIBSOURCE)/SmoothValue.cpp
+# C_SRC  += $(LIBSOURCE)/fastpow.c $(LIBSOURCE)/fastlog.c $(LIBSOURCE)/system_tables.cpp
+C_SRC   += Libraries/KissFFT/kiss_fft.c
+C_SRC   += $(wildcard $(GENSOURCE)/*.c)
+CPP_SRC += $(wildcard $(GENSOURCE)/*.cpp)
+C_SRC   += $(wildcard $(PATCHSOURCE)/*.c)
+CPP_SRC += $(wildcard $(PATCHSOURCE)/*.cpp)
+WEBDIR   = $(BUILD)/web
+
+EMCCFLAGS += -s WASM=0
+
+CPPFLAGS =
+CFLAGS = $(EMCCFLAGS)
+CXXFLAGS = $(EMCCFLAGS) -std=gnu++11
+LDFLAGS = $(EMCCFLAGS)
+
+EMCC_OBJS = $(addprefix $(BUILD)/, $(notdir $(CPP_SRC:.cpp=.o)))
+EMCC_OBJS += $(addprefix $(BUILD)/, $(notdir $(C_SRC:.c=.o)))
+
+# Set up search path
+vpath %.cpp $(SOURCE)
+vpath %.c $(SOURCE)
+vpath %.s $(SOURCE)
+vpath %.cpp $(LIBSOURCE)
+vpath %.c $(LIBSOURCE)
+vpath %.s $(LIBSOURCE)
+vpath %.cpp $(PATCHSOURCE)
+vpath %.c $(PATCHSOURCE)
+vpath %.s $(PATCHSOURCE)
+vpath %.cpp $(GENSOURCE)
+vpath %.c $(GENSOURCE)
+vpath %.s $(GENSOURCE)
+vpath %.cpp WebSource
+vpath %.c Libraries/KissFFT
 
 ifeq ($(CONFIG),Debug)
 EMCCFLAGS += -s ASSERTIONS=1
@@ -36,21 +62,25 @@ endif
 #CLOSURE = java -jar Tools/node_modules/google-closure-compiler/compiler.jar --language_in=ECMASCRIPT5
 UGLIFYJS = Tools/node_modules/uglifyjs/bin/uglifyjs
 
-online:
-	@echo "$(ONLINE_INCLUDES)" > $(BUILD)/patch.h
-	@echo "$(ONLINE_REGISTER)" > $(BUILD)/patch.cpp
-	@echo '.string "OnlineCompiler"' > $(BUILD)/progname.s
-	@$(MAKE) $(BUILD)/$(TARGET).syx
-	@cp $(BUILD)/$(TARGET).syx $(BUILD)/online.syx
-
-$(WEBDIR)/$(TARGET).js: $(EMCC_SRC)
+$(WEBDIR)/$(TARGET).js: $(EMCC_OBJS)
 	@mkdir -p $(WEBDIR)
-	$(EMCC) $(EMCCFLAGS) $(EMCC_SRC) -o $(WEBDIR)/$(TARGET).js
+	$(EMCC) $(LDFLAGS) $(EMCC_OBJS) -o $(WEBDIR)/$(TARGET).js
 	@cp WebSource/*.js WebSource/*.html $(WEBDIR)
 
 $(WEBDIR)/%.min.js: $(WEBDIR)/%.js
 	@$(UGLIFYJS) -o $@ $<
 #	$(CLOSURE) --js_output_file=$@ $<
 
-web: $(WEBDIR)/$(TARGET).js
-minify: $(WEBDIR)/$(TARGET).min.js
+
+# compile and generate dependency info
+$(BUILD)/%.o: %.c
+	$(EMCC) -c $(CPPFLAGS) $(CFLAGS) $< -o $@
+	$(EMCC) -MM -MT"$@" $(CPPFLAGS) $(CFLAGS) $< > $(@:.o=.d)
+
+$(BUILD)/%.o: %.cpp
+	$(EMCC) -c $(CPPFLAGS) $(CXXFLAGS) $< -o $@
+	$(EMCC) -MM -MT"$@" $(CPPFLAGS) $(CXXFLAGS) $< > $(@:.o=.d)
+
+web: $(WEBDIR)/$(TARGET).js ;
+
+minify: $(WEBDIR)/$(TARGET).min.js ;
