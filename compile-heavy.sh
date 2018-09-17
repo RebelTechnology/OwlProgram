98d33ee39b789836778ac1d4b990a36987fa811c
Martin Klang <martin@rebel-it.com>
2018-09-17 15:59:55 +0100

    output all targets except js

--- compile-heavy.sh
+++ compile-heavy.sh
@@ -3,11 +3,12 @@
 DIR=`dirname "$0"`
 HVCC=$DIR/Tools/hvcc/hvcc.py
 FILENAME=`basename $MAIN_FILENAME`
+TARGETS="c unity wwise bela fabric pdext vst2"
 
 mkdir -p $BUILD_DIR
 rm -f $BUILD_DIR/c/HvHeavy.cpp
 
-python2.7 $HVCC $SOURCE_DIR/$FILENAME -g c -n "$PATCHNAME" -p $SOURCE_DIR -o $BUILD_DIR
+python2.7 $HVCC $SOURCE_DIR/$FILENAME -g $TARGETS -n "$PATCHNAME" -p $SOURCE_DIR -o $BUILD_DIR
 
 # hvcc always returns 0 so this doesn't work: if [ $? -eq 0 ]
 if [ -f $BUILD_DIR/c/HvHeavy.cpp ]
