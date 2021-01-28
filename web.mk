b46d145151442129b5cee30ec1554e8a763fd39a
Martin Klang <martin@rebel-it.com>
2021-01-28 17:56:02 +0100

    updated compilation flags to be SOUL compatible (C++14)

--- web.mk
+++ web.mk
@@ -7,13 +7,11 @@ EMCC      ?= emcc
 EMAR      ?= emar
 EMCCFLAGS += -fno-rtti -fno-exceptions
 # EMCCFLAGS += -s ASSERTIONS=1 -Wall
-EMCCFLAGS += -Dnullptr=NULL
 EMCCFLAGS += -I$(SOURCE) -I$(PATCHSOURCE) -I$(LIBSOURCE) -I$(GENSOURCE) -I$(BUILD)
 EMCCFLAGS += -I$(BUILD)/Source
 EMCCFLAGS +=  -ILibraries -ILibraries/KissFFT -DHV_SIMD_NONE
 EMCCFLAGS += -Wno-warn-absolute-paths
 EMCCFLAGS += -Wno-unknown-warning-option
-EMCCFLAGS += -Wno-c++11-extensions
 EMCCFLAGS += --memory-init-file 0 # don't create separate memory init file .mem
 EMCCFLAGS += -s EXPORTED_FUNCTIONS="['_WEB_setup','_WEB_setParameter','_WEB_getParameter','_WEB_processBlock','_WEB_getPatchName','_WEB_getParameterName','_WEB_getMessage','_WEB_getStatus','_WEB_getButtons','_WEB_setButton', '_WEB_processMidi', '_malloc']"
 EMCCFLAGS += -s "EXTRA_EXPORTED_RUNTIME_METHODS=['cwrap']"
@@ -32,7 +30,7 @@ EMCCFLAGS += -s WASM=0 # disables wasm output
 
 CPPFLAGS =
 CFLAGS   = $(EMCCFLAGS) -std=gnu11
-CXXFLAGS = $(EMCCFLAGS) -std=gnu++11
+CXXFLAGS = $(EMCCFLAGS) -std=gnu++14
 LDFLAGS  = $(EMCCFLAGS)
 
 EMCC_OBJS = $(addprefix $(WEBDIR)/, $(notdir $(CPP_SRC:.cpp=.o)))
