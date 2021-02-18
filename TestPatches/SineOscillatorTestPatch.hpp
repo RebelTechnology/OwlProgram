74f358b3e90110c8a9efc53757aa7260f08b3bf8
Martin Klang <martin@rebel-it.com>
2021-02-18 10:03:07 +0100

    added test

--- TestPatches/SineOscillatorTestPatch.hpp
+++ TestPatches/SineOscillatorTestPatch.hpp
@@ -30,6 +30,27 @@ public:
       CHECK_EQUAL(osc->getFrequency(), 321); // check that frequency is the same
       SineOscillator::destroy(osc);      
     }
-  }
+    {
+      TEST("compareBlockAndSampleBased");
+      SineOscillator* osc1 = SineOscillator::create(480, 48000);
+      SineOscillator* osc2 = SineOscillator::create(480, 48000);
+      FloatArray s1 = FloatArray::create(1000);
+      FloatArray s2 = FloatArray::create(1000);
+      osc1->generate(s1);
+      for(size_t i=0; i<1000; ++i)
+	s2[i] = osc2->generate();
+      for(size_t i=0; i<1000; ++i)
+	CHECK_CLOSE(s1[i], s2[i], 0.00002);
+      osc1->generate(s1);
+      for(size_t i=0; i<1000; ++i)
+	s2[i] = osc2->generate();
+      for(size_t i=0; i<1000; ++i)
+	CHECK_CLOSE(s1[i], s2[i], 0.00002);
+      FloatArray::destroy(s1);      
+      FloatArray::destroy(s2);      
+      SineOscillator::destroy(osc1);      
+      SineOscillator::destroy(osc2);      
+    }
+}
 };
 
