9c3f4b2f356f1b27a4e0f5ee9a86fa5ee7557efa
Martin Klang <martin@rebel-it.com>
2021-03-10 11:37:54 +0100

    fixed state management

--- LibSource/StateVariableFilter.h
+++ LibSource/StateVariableFilter.h
@@ -126,31 +126,29 @@ public:
   StateVariableFilter(float sr): AbstractStateVariableFilter(sr) {}
 
   float process(float v0){
-    mV3 = v0 - mIc2eq;
-    mV1 = m_a1 * mIc1eq + m_a2*mV3;
-    mV2 = mIc2eq + m_a2 * mIc1eq + m_a3 * mV3;
-    mIc1eq = 2. * mV1 - mIc1eq;
-    mIc2eq = 2. * mV2 - mIc2eq;
-    return m_m0 * v0 + m_m1 * mV1 + m_m2 * mV2;
+    float v3 = v0 - mIc2eq;
+    float v1 = m_a1 * mIc1eq + m_a2*v3;
+    float v2 = mIc2eq + m_a2 * mIc1eq + m_a3 * v3;
+    mIc1eq = 2. * v1 - mIc1eq;
+    mIc2eq = 2. * v2 - mIc2eq;
+    return m_m0 * v0 + m_m1 * v1 + m_m2 * v2;
   }
 
   void process(FloatArray input, FloatArray output){
     size_t nFrames = input.getSize();
+    float v0, v1, v2, v3;
     for(size_t s = 0; s < nFrames; s++){
-      const float v0 = input[s];
-      mV3 = v0 - mIc2eq;
-      mV1 = m_a1 * mIc1eq + m_a2*mV3;
-      mV2 = mIc2eq + m_a2 * mIc1eq + m_a3 * mV3;
-      mIc1eq = 2. * mV1 - mIc1eq;
-      mIc2eq = 2. * mV2 - mIc2eq;
-      output[s] = m_m0 * v0 + m_m1 * mV1 + m_m2 * mV2;
+      v0 = input[s];
+      v3 = v0 - mIc2eq;
+      v1 = m_a1 * mIc1eq + m_a2*v3;
+      v2 = mIc2eq + m_a2 * mIc1eq + m_a3 * v3;
+      mIc1eq = 2. * v1 - mIc1eq;
+      mIc2eq = 2. * v2 - mIc2eq;
+      output[s] = m_m0 * v0 + m_m1 * v1 + m_m2 * v2;
     }
   }
 
   void reset() {
-    mV1 = 0.;
-    mV2 = 0.;
-    mV3 = 0.;
     mIc1eq = 0.;
     mIc2eq = 0.;
   }
@@ -165,9 +163,6 @@ public:
   
 private:
   // state
-  float mV1 = 0.;
-  float mV2 = 0.;
-  float mV3 = 0.;
   float mIc1eq = 0.;
   float mIc2eq = 0.;
 };
@@ -175,18 +170,15 @@ private:
 class MultiStateVariableFilter : public AbstractStateVariableFilter, MultiSignalProcessor {
 private:
   size_t mChannels;
-  // state
   float* mState;
-  static constexpr size_t STATE_VARIABLES_PER_CHANNEL = 5;
+  static constexpr size_t STATE_VARIABLES_PER_CHANNEL = 2;
 public:
   MultiStateVariableFilter(float sr) :
     AbstractStateVariableFilter(sr), 
     mChannels(0), mState(NULL) {}
 
-  MultiStateVariableFilter(float sr, size_t channels,
-			   float* state) :
-    AbstractStateVariableFilter(sr),
-    mChannels(channels), mState(state) {}
+  MultiStateVariableFilter(float sr, size_t channels, float* state) :
+    AbstractStateVariableFilter(sr), mChannels(channels), mState(state) {}
 
   void process(AudioBuffer &input, AudioBuffer &output){
     size_t len = min((int)mChannels, min(input.getChannels(), output.getChannels()));
@@ -195,20 +187,21 @@ public:
       FloatArray in = input.getSamples(ch);
       FloatArray out = output.getSamples(ch);
       size_t nFrames = in.getSize();
-      float mV1 = *state++;
-      float mV2 = *state++;
-      float mV3 = *state++;
-      float mIc1eq = *state++;
-      float mIc2eq = *state++;
+      float v0, v1, v2, v3;
+      float mIc1eq = state[0];
+      float mIc2eq = state[1];
       for(size_t s = 0; s < nFrames; s++){
-	float v0 = in[s];
-	mV3 = v0 - mIc2eq;
-	mV1 = m_a1 * mIc1eq + m_a2*mV3;
-	mV2 = mIc2eq + m_a2 * mIc1eq + m_a3 * mV3;
-	mIc1eq = 2. * mV1 - mIc1eq;
-	mIc2eq = 2. * mV2 - mIc2eq;
-	out[s] = m_m0 * v0 + m_m1 * mV1 + m_m2 * mV2;
+	v0 = in[s];
+	v3 = v0 - mIc2eq;
+	v1 = m_a1 * mIc1eq + m_a2*v3;
+	v2 = mIc2eq + m_a2 * mIc1eq + m_a3 * v3;
+	mIc1eq = 2. * v1 - mIc1eq;
+	mIc2eq = 2. * v2 - mIc2eq;
+	out[s] = m_m0 * v0 + m_m1 * v1 + m_m2 * v2;
       }
+      state[0] = mIc1eq;
+      state[1] = mIc2eq;
+      state += STATE_VARIABLES_PER_CHANNEL;
     }
   }
 
