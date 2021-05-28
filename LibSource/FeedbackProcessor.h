5b9795c68f98dd5bf4551599a307c6d7bf628f3b
Martin Klang <martin@rebel-it.com>
2021-05-28 22:08:34 +0200

    added create() methods, fixed destroy()

--- LibSource/FeedbackProcessor.h
+++ LibSource/FeedbackProcessor.h
@@ -26,6 +26,10 @@ public:
     Processor::process(input, output);
     feedback_buffer.copyFrom(output);
   }
+  template <typename... Args>
+  static FeedbackSignalProcessor<Processor>* create(size_t blocksize, Args&&... args){
+    return new FeedbackSignalProcessor<Processor>(FloatArray::create(blocksize), std::forward<Args>(args)...);
+  }
   static void destroy(FeedbackSignalProcessor<Processor>* obj){
     FloatArray::destroy(obj->feedback_buffer);
     Processor::destroy(obj);
@@ -64,9 +68,13 @@ public:
     feedback_left.copyFrom(output_left);
     feedback_right.copyFrom(output_right);
   }
+  static StereoFeedbackProcessor* create(SignalProcessor* left, SignalProcessor* right, size_t blocksize){
+    return new StereoFeedbackProcessor(left, right, FloatArray::create(blocksize), FloatArray::create(blocksize));
+  }
   static void destroy(StereoFeedbackProcessor* obj){
     FloatArray::destroy(obj->feedback_left);
     FloatArray::destroy(obj->feedback_right);
+    delete obj;
   }
 };
 
@@ -106,9 +114,13 @@ public:
     feedback_right.copyFrom(output_left);
     dc_right.process(feedback_right);
   }
+  static PingPongFeedbackProcessor* create(SignalProcessor* left, SignalProcessor* right, size_t blocksize){
+    return new PingPongFeedbackProcessor(left, right, FloatArray::create(blocksize), FloatArray::create(blocksize));
+  }
   static void destroy(PingPongFeedbackProcessor* obj){
     FloatArray::destroy(obj->feedback_left);
     FloatArray::destroy(obj->feedback_right);
+    delete obj;
   }
 };
 
