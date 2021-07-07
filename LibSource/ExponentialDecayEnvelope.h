51cf822fb7e1405c99ebadbd643b8453c5a9bf70
Martin Klang <martin@rebel-it.com>
2021-07-07 12:49:46 +0200

    changed time basis calculation

--- LibSource/ExponentialDecayEnvelope.h
+++ LibSource/ExponentialDecayEnvelope.h
@@ -4,10 +4,14 @@
 #include "Envelope.h"
 
 class ExponentialDecayEnvelope : public Envelope {
-private:
+protected:
   float sr;
   float value;
   float incr;
+  float calculateMultiplier(float startLevel, float endLevel, size_t lengthInSamples) {
+    // Ref: Christian Schoenebeck http://www.musicdsp.org/showone.php?id=189
+    return 1.0 + (logf(endLevel) - logf(startLevel)) / (lengthInSamples+20);
+  }
 public:
   ExponentialDecayEnvelope(float sr=48000): sr(sr), value(0), incr(0){}
   using Envelope::gate;
@@ -17,14 +21,11 @@ public:
   void setSampleRate(float sampleRate){
     sr = sampleRate;
   }
-  void setRate(float r){
-    if(r < 0)
-      incr = 1.0f - 100*(1/(1-r))/sr;
-    else
-      incr = 1.0f + 100*r/sr;
+  void setDecaySamples(float samples){ // set decay time in samples
+    incr = calculateMultiplier(1, 0.0001, samples);
   }
-  void setDecay(float d){
-    setRate(-(d+1/sr));
+  void setDecay(float sec){ // set decay time in seconds to reach -80dB
+    incr = calculateMultiplier(1, 0.0001, sec*sr);
   }
   void trigger(bool state, int triggerDelay){
     if(state)
@@ -53,11 +54,6 @@ public:
   }  
   static ExponentialDecayEnvelope* create(float sr){
     return new ExponentialDecayEnvelope(sr);
-  }
-  static ExponentialDecayEnvelope* create(float rate, float sr){
-    ExponentialDecayEnvelope* env = new ExponentialDecayEnvelope(sr);
-    env->setDecay(rate);
-    return env;
   }
   static void destroy(ExponentialDecayEnvelope* env){
     delete env;
