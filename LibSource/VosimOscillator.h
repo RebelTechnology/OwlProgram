da791001bd1fe559947dd004caa1974ccbf3baa7
Martin Klang <martin@rebel-it.com>
2021-07-12 16:47:21 +0200

    changed to use oscillator template

--- LibSource/VosimOscillator.h
+++ LibSource/VosimOscillator.h
@@ -2,36 +2,32 @@
 #define __VosimOscillator_h
 
 #include "Oscillator.h"
+#include "PolyBlepOscillator.h"
 
 /**
  * VOSIM-type oscillator based on Rob Hordijk's Nord Modular patch.
  * ref: https://electro-music.com/nm_classic/015_workshops/Clavia/NordModularWorkshops&Threads/WerkMap/WorkShops/Hordijk1999-2000/VOSIM.html
 */
-class VosimOscillator : public Oscillator {
+class VosimOscillator : public OscillatorTemplate<VosimOscillator> {
 private:
-  const float fs;
   SineOscillator* formant1;
   SineOscillator* formant2;
-  float phase = 0;
-  float incr = 0;
 public:
-  VosimOscillator(float samplerate, SineOscillator* osc2, SineOscillator* osc1) :
-    fs(samplerate), formant1(osc1) , formant2(osc2) {}
-  float getFrequency(){
-    return incr*fs;
+  VosimOscillator(SineOscillator* osc2, SineOscillator* osc1) :
+    formant1(osc1) , formant2(osc2) {}
+  static constexpr float begin_phase = 0;
+  static constexpr float end_phase = 1;
+  float waveshape = 0;
+  float getSample(){
+    float square = phase < 0.5 ? 0.0f : 1.0f;
+    float saw = phase;
+    return 1 - ( saw + (square - saw) * waveshape );
+    // return 1 - ( phase - polyblep(phase, incr));
+    // return 1 - phase;
   }
-  void setFrequency(float freq){
-    incr = freq/fs;
-  }
-  void reset(){
-    phase = 0.0f;
-  }
-  void setPhase(float ph){
-    phase = 1-ph/(2*M_PI);
-  }
-  float getPhase(){
-    return (1-phase)*2*M_PI;
-  }
+  void setWaveshape(float value){
+    waveshape = value;
+  }    
   float getFormant1(){
     return formant1->getFrequency();
   }
@@ -46,16 +42,14 @@ public:
   }
   using Oscillator::generate;
   float generate(float fm){
+    float sample = getSample() * (1 - 0.4*waveshape);
     float s1 = formant1->generate();
     float s2 = formant2->generate();
-    float sample = (s1*s1 + s2*s2)*phase - 1;
+    sample = (s1*s1 + s2*s2)*sample;
     float dt = incr * (1 + fm);
-    // sample = 2*phase-1;
-    // sample -= polyblep(phase, dt);
-    // polyblep works on sawtooth but doesn't help vosim
-    phase -= dt;
-    if(phase <= 0.0){
-      phase += 1.0;
+    phase += incr * (1 + fm);
+    if(phase >= end_phase){
+      phase -= (end_phase - begin_phase);
 #if 0
       float ph = getPhase();
       formant1->setPhase(ph);
@@ -68,28 +62,14 @@ public:
     return sample;
   }
   float generate(){
-    float s1 = formant1->generate();
-    float s2 = formant2->generate();
-    float sample = (s1*s1 + s2*s2)*phase - 1;
-    // sample -= polyblep(1-phase, incr);
-    phase -= incr;
-    if(phase <= 0.0){
-      phase += 1.0;
-#if 0
-      float ph = getPhase();
-      formant1->setPhase(ph);
-      formant2->setPhase(ph);
-#else
-      formant1->reset();
-      formant2->reset();
-#endif
-    }
-    return sample;
+    return generate(0);
   }
   static VosimOscillator* create(float sr){
     SineOscillator* f1 = SineOscillator::create(sr);
     SineOscillator* f2 = SineOscillator::create(sr);
-    return new VosimOscillator(sr, f1, f2);
+    VosimOscillator* osc = new VosimOscillator(f1, f2);
+    osc->setSampleRate(sr);
+    return osc;
   }
   static void destroy(VosimOscillator* obj){
     SineOscillator::destroy(obj->formant1);
