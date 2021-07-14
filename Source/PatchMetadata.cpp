a59411fbb1b3a948416c9d1b1dec097fec321c2d
Martin Klang <martin@rebel-it.com>
2021-07-14 14:00:13 +0200

    fix default parameter / button count

--- Source/PatchMetadata.cpp
+++ Source/PatchMetadata.cpp
@@ -5,7 +5,7 @@
 __weak const char* PatchMetadata::name = PATCHNAME;
 __weak const int PatchMetadata::channels_in = 2;
 __weak const int PatchMetadata::channels_out = 2;
-__weak const int PatchMetadata::parameter_count = 2;
-__weak const int PatchMetadata::button_count = 2;
+__weak const int PatchMetadata::parameter_count = 0;
+__weak const int PatchMetadata::button_count = 0;
 __weak const PatchMetadata::Control PatchMetadata::parameters[] = {};
 __weak const PatchMetadata::Control PatchMetadata::buttons[] = {};
