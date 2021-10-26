6ac4d0159324fadcfe581a7c4d69d06755311d4e
Martin Klang <martin@rebel-it.com>
2021-10-26 17:46:16 +0200

    removed inconsistently used override declarations

--- Source/MemoryBuffer.hpp
+++ Source/MemoryBuffer.hpp
@@ -11,17 +11,17 @@ protected:
 public:
   MemoryBuffer(float* buf, size_t ch, size_t sz): buffer(buf), channels(ch), size(sz) {}
   virtual ~MemoryBuffer(){}
-  void clear() override {
+  void clear() {
     memset(buffer, 0, size*channels*sizeof(float));
   }
-  FloatArray getSamples(int channel) override {
+  FloatArray getSamples(int channel) {
     ASSERT((size_t)channel < channels, "Invalid channel");
     return FloatArray(buffer+channel*size, size);
   }
-  int getChannels() override {
+  int getChannels() {
     return channels;
   }
-  int getSize() override {
+  int getSize() {
     return size;
   }
 };
