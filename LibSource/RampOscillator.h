6c5eccbf29fe254330a65be60356e6d2936c0918
Martin Klang <martin@rebel-it.com>
2021-12-16 02:27:41 +0100

    added InvertedRampOscillator and constructors

--- LibSource/RampOscillator.h
+++ LibSource/RampOscillator.h
@@ -10,8 +10,28 @@ class RampOscillator : public OscillatorTemplate<RampOscillator> {
 public:
   static constexpr float begin_phase = -1;
   static constexpr float end_phase = 1;
+  RampOscillator(){}
+  RampOscillator(float sr){
+    setSampleRate(sr);
+  }  
   float getSample(){
     return phase;
+  }
+};
+
+/**
+ * Inverted ramp oscillator generates falling output values from 1 to -1.
+ */
+class InvertedRampOscillator : public OscillatorTemplate<InvertedRampOscillator> {
+public:
+  static constexpr float begin_phase = -1;
+  static constexpr float end_phase = 1;
+  InvertedRampOscillator(){}
+  InvertedRampOscillator(float sr){
+    setSampleRate(sr);
+  }  
+  float getSample(){
+    return -phase;
   }
 };
 
