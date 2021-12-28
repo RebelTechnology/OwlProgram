0c4ce771a001ce6aca757c2fd6449ae5566f2e5c
Martin Klang <martin@rebel-it.com>
2021-12-28 18:48:05 +0100

    added back deprecated SquareOscillator for backwards compatibility

---
+++ LibSource/SquareOscillator.h
@@ -0,0 +1,12 @@
+#ifndef __SquareOscillator_h
+#define __SquareOscillator_h
+
+#include "SquareWaveOscillator.h"
+
+#warning "SquareOscillator.h is deprecated, use SquareWaveOscillator.h instead"
+
+typedef SquareWaveOscillator SquareOscillator;
+typedef InvertedSquareWaveOscillator InvertedSquareOscillator;
+typedef AntialiasedSquareWaveOscillator AntialiasedSquareOscillator;
+
+#endif /* __SquareOscillator_h */
