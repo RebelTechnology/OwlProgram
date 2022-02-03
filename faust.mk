da8acf4823278a3cf9ab38c317bc4b461520d5fb
Martin Klang <martin@rebel-it.com>
2022-02-03 21:59:46 +0100

    different faust flags for (possibly) better performance

--- faust.mk
+++ faust.mk
@@ -4,14 +4,14 @@ PATCHSOURCE ?= $(BUILDROOT)/PatchSource
 FAUSTSOURCE ?= $(BUILDROOT)/FaustSource
 
 FAUSTCC ?= faust
-FAUSTFLAGS ?= -light
+FAUSTFLAGS ?= -mem -light -scal -exp10 -mcd 0
 
 faust: $(BUILD)/Source/FaustPatch.hpp
 
 $(BUILD)/Source/FaustPatch.hpp: $(PATCHSOURCE)/$(FAUST).dsp
-	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -mem -a FaustSource/owl.cpp -fm faustmath.h $< $(FAUSTSOURCE)/owl.lib -o $@
+	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -a FaustSource/owl.cpp -fm faustmath.h $< $(FAUSTSOURCE)/owl.lib -o $@
 	@cp $(FAUSTSOURCE)/*.h $(BUILD)/Source
 
 $(BUILD)/Source/%Patch.hpp: $(PATCHSOURCE)/%.dsp
-	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -mem -a $(FAUSTSOURCE)/owl.cpp -fm faustmath.h $< -o $@
+	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -a $(FAUSTSOURCE)/owl.cpp -fm faustmath.h $< -o $@
 
