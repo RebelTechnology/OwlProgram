5683675ca3ac73699c55626b1822e5634833c8ae
Martin Klang <martin@rebel-it.com>
2022-02-08 22:02:54 +0100

    update default faust compilation options

--- faust.mk
+++ faust.mk
@@ -4,14 +4,14 @@ PATCHSOURCE ?= $(BUILDROOT)/PatchSource
 FAUSTSOURCE ?= $(BUILDROOT)/FaustSource
 
 FAUSTCC ?= faust
-FAUSTFLAGS ?= -mem -light -scal -exp10 -mcd 0
+FAUSTFLAGS ?= -mem -light -scal -inpl # -exp10 -mcd 0 -ftz 2
 
 faust: $(BUILD)/Source/FaustPatch.hpp
 
 $(BUILD)/Source/FaustPatch.hpp: $(PATCHSOURCE)/$(FAUST).dsp
-	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -a FaustSource/owl.cpp -fm faustmath.h $< $(FAUSTSOURCE)/owl.lib -o $@
+	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -a FaustSource/owl.cpp -fm faustmath.h $< $(FAUSTSOURCE)/owl.lib -o $@
 	@cp $(FAUSTSOURCE)/*.h $(BUILD)/Source
 
 $(BUILD)/Source/%Patch.hpp: $(PATCHSOURCE)/%.dsp
-	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -inpl -a $(FAUSTSOURCE)/owl.cpp -fm faustmath.h $< -o $@
+	@$(FAUSTCC) $(FAUSTFLAGS) -I $(PATCHSOURCE) -i -a $(FAUSTSOURCE)/owl.cpp -fm faustmath.h $< -o $@
 
