41be6911e8697a37ea900e512ca380375d971ac9
Martin Klang <martin@rebel-it.com>
2022-04-10 12:52:13 +0200

    added InterpolatingWavetableOscillator

--- LibSource/WavetableOscillator.h
+++ LibSource/WavetableOscillator.h
@@ -49,4 +49,38 @@ public:
   using OscillatorTemplate<AntialiasedWavetableOscillator>::generate;  
 };
 
+template<InterpolationMethod im = LINEAR_INTERPOLATION>
+class InterpolatingWavetableOscillator : public OscillatorTemplate<InterpolatingWavetableOscillator<im>> {
+private:
+  InterpolatingCircularFloatBuffer<im> wave = NULL;
+public:
+  static constexpr float begin_phase = 0;
+  static constexpr float end_phase = 1;
+  InterpolatingWavetableOscillator(){}
+  InterpolatingWavetableOscillator(float sr){
+    OscillatorTemplate<InterpolatingWavetableOscillator<im>>::setSampleRate(sr);
+  }
+  InterpolatingWavetableOscillator(float sr, FloatArray data)
+    : wave(data.getData(), data.getSize()) {
+    OscillatorTemplate<InterpolatingWavetableOscillator<im>>::setSampleRate(sr);
+  }
+  FloatArray getWavetable(){
+    return FloatArray(wave.getData(), wave.getSize());
+  }
+  void setWavetable(FloatArray wavetable){
+    wave.setData(wavetable.getData(), wavetable.getSize());
+  }
+  float getSample(){
+    float index = OscillatorTemplate<InterpolatingWavetableOscillator<im>>::phase * wave.getSize();
+    return wave.readAt(index);
+  }
+  static InterpolatingWavetableOscillator<im>* create(float sr, size_t length){
+    return new InterpolatingWavetableOscillator<im>(sr, FloatArray::create(length));
+  }
+  static void destroy(InterpolatingWavetableOscillator<im>* obj){
+    FloatArray::destroy(obj->getWavetable());
+    delete obj;
+  }
+};
+
 #endif /* __WavetableOscillator_h__ */
